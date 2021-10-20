7d30544369ad634190d983a478a72e7389588070
pmall <pierre.mallinjoud@gmail.com>
2021-10-20 17:50:31 +0200

    use sharp library to produce images

--- src/cmd/images.ts
+++ src/cmd/images.ts
@@ -1,5 +1,50 @@
-export const images = (dir: string) => {
+import fs from 'fs'
+import path from 'path'
+import sharp from 'sharp'
+
+type Attribute = {
+    file: string
+}
+
+const paths = {
+    attributes: `./attributes.json`,
+    traits: `./traits`,
+    images: `./images`,
+}
+
+export const images = async (dir: string) => {
     const root = dir.replace(/\/+$/, '')
 
-    console.log(root)
+    console.log(`Generating images from ${root}/attributes.json`)
+
+    process.chdir(root)
+
+    if (!fs.existsSync(paths.attributes)) {
+        throw new Error(`no ${paths.attributes} file found`)
+    }
+
+    const raw = fs.readFileSync(paths.attributes, 'utf8')
+
+    const attributes: Attribute[][] = JSON.parse(raw)
+
+    // remove all .png files of the images folder
+    const files = fs.readdirSync(paths.images)
+
+    for (const file of files) {
+        if (path.extname(file) == '.png') {
+            fs.unlinkSync(`${paths.images}/${file}`)
+        }
+    }
+
+    // generate one image per attributes
+    for (let i = 0; i < attributes.length; i++) {
+        const xs = attributes[i]
+        const x = xs.shift()
+
+        if (!x) throw new Error(`No trait for image ${i}`)
+
+        sharp(x.file)
+            .composite(xs.map(({ file }) => ({ input: file })))
+            .toFile(`${paths.images}/${i + 1}.png`)
+    }
 }
